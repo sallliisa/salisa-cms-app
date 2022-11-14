0478c8413b04ee5dea6b9135a5218ab342c13f30
salisa <sodiumcation@gmail.com>
2022-11-14 10:05:57 +0700

    new font, fixed css, edit and delete articles

--- src/config/config.js
+++ src/config/config.js
@@ -4,10 +4,16 @@ dotenv.config();
 
 const HOST_ADDRESS = process.env.HOST_ADDRESS || "";
 const HOST_PORT = process.env.HOST_PORT || "";
+const API_ADDRESS = process.env.API_ADDRESS || "";
+const API_PORT = process.env.API_PORT || "";
 
 export const config = {
   server: {
     address: HOST_ADDRESS,
     port: HOST_PORT,
+  },
+  api: {
+    address: API_ADDRESS,
+    port: API_PORT,
   },
 };
--- src/services/webServices.js
+++ src/services/webServices.js
@@ -1,19 +1,40 @@
+import { config } from "../config/config.js";
 import got from "got";
 
 export const services = {
   getAllArticles: async () => {
-    const data = await got.get(`http://127.0.0.1:8000/api/articles`).json();
+    const data = await got
+      .get(`http://${config.api.address}:${config.api.port}/api/articles`)
+      .json();
     return data.articles;
   },
   getArticle: async (articleId) => {
     const data = await got
-      .get(`http://127.0.0.1:8000/api/article/${articleId}`)
+      .get(
+        `http://${config.api.address}:${config.api.port}/api/article/${articleId}`
+      )
       .json();
     return data.article;
   },
   newArticle: async (data) => {
-    await got.post(`http://127.0.0.1:8000/api/newarticle/`, {
-      json: data,
-    });
+    await got.post(
+      `http://${config.api.address}:${config.api.port}/api/newarticle/`,
+      {
+        json: data,
+      }
+    );
+  },
+  editArticle: async (articleId, data) => {
+    await got.post(
+      `http://${config.api.address}:${config.api.port}/api/editarticle/${articleId}`,
+      {
+        json: data,
+      }
+    );
+  },
+  deleteArticle: async (articleId) => {
+    await got.delete(
+      `http://${config.api.address}:${config.api.port}/api/deletearticle/${articleId}`
+    );
   },
 };
